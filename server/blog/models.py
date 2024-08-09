8e8ad5de3ee3f291a0e5ea03aadc271151aa202c
sachhabria <sach.chhabria@gmail.com>
2024-08-08 17:48:36 -0700

    Define Tag, Post, Comment models in blog app

--- server/blog/models.py
+++ server/blog/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    image = models.URLField(max_length=500)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    image = models.URLField(max_length=500)
+    caption = models.TextField()
+    created = models.DateTimeField()
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.body
